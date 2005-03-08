984307e103b3877934570e1aec758d040383ff8d
hawkmoon <hawkmoon@95ac9d48-2a10-4898-8b55-59519d9e2ec5>
2005-03-08 14:21:55 +0000

    Initial revision

---
+++ pgGUID/newid.h
@@ -0,0 +1,14 @@
+/*
+ *  newid.h
+ *  guid
+ *
+ *  Created by Andy Satori on 9/29/04.
+ *  Copyright 2004 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <stdlib.h>
+
+char* getNewGUID();
+
+
