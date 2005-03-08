0f0d28b2665bc771dc362bcf8e603c25d35cfd86
nobody <nobody@95ac9d48-2a10-4898-8b55-59519d9e2ec5>
2005-03-08 21:50:11 +0000

    This commit was manufactured by cvs2svn to create branch 'vendor'.

---
+++ CreateUser/PGNewUserUI.h
@@ -0,0 +1,44 @@
+//
+//  PGNewUserUI.h
+//  Create User
+//
+//  Created by Andy Satori on 2/21/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <pgCocoaDB/Connection.h>
+#import <pgCocoaDB/Databases.h>
+#import <pgCocoaDB/RecordSet.h>
+
+
+@interface PGNewUserUI : NSObject 
+{
+    IBOutlet NSButton *back;
+    IBOutlet NSButton *next;
+    IBOutlet NSSecureTextField *password;
+    IBOutlet NSTextField *port;
+    IBOutlet NSTextView *resultOutput;
+    IBOutlet NSProgressIndicator *resultStatus;
+    IBOutlet NSTextField *server;
+    IBOutlet NSTextField *user;
+    IBOutlet NSWindow *window;
+    IBOutlet NSTabView *tabs;
+
+    IBOutlet NSTextField *newLogin;
+    IBOutlet NSTextField *newUID;
+    IBOutlet NSTextField *newPassword;
+    IBOutlet NSTextField *newConfPassword;
+    IBOutlet NSTextField *newExpirationDate;
+    IBOutlet NSPopUpButton *groups;
+	IBOutlet NSButton *allowCreateDB;
+	IBOutlet NSButton *allowCreateUser;
+	IBOutlet NSButton *versionSevenFeaturesOnly;
+	
+	Connection *_conn;
+}
+- (IBAction)onBack:(id)sender;
+- (IBAction)onCancel:(id)sender;
+- (IBAction)onNext:(id)sender;
+
+@end
---
+++ QueryTool/DataSource.m
@@ -0,0 +1,65 @@
+//
+//  DataSource.m
+//
+//  Created by Andy Satori on Sun 02/08/04 05:38 PM
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "DataSource.h"
+
+@implementation DataSource
+
+- (id)init
+{
+    [super init];
+    
+    items = [[NSMutableArray alloc] init];
+    [items retain];
+    
+    return self;
+}
+
+// collection management
+
+- (NSMutableDictionary *)addItem
+{
+    NSMutableDictionary *newItem = [[NSMutableDictionary alloc] init];
+    [newItem retain];
+    
+    [items addObject: newItem];
+    return newItem;    
+}
+
+- (void)removeItemAtIndex:(int)index
+{
+    [items removeObjectAtIndex:index];
+}
+
+- (NSMutableDictionary *)itemAtIndex:(int)index
+{
+    return [items objectAtIndex:index];
+}
+
+- (int)count
+{
+    return [items count];
+}
+
+// table view data source methods
+
+- (int)numberOfRowsInTableView:(NSTableView *)aTableView
+{
+    return [items count];
+}
+
+- (id)tableView:(NSTableView *)aTableView 
+    objectValueForTableColumn:(NSTableColumn *)aTableColumn 
+    row:(int)rowIndex 
+{
+    NSString *ident = [aTableColumn identifier];
+    NSMutableDictionary *anItem = [items objectAtIndex:rowIndex];
+    return [anItem valueForKey:ident];
+}
+
+@end
