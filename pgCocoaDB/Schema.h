aa49330b3063b9e341b2e6a3c5a2751fe74d1260
Neil Tiffin <neilt@users.noreply.github.com>
2006-03-05 00:23:55 +0000

    Schema work in progress, not working.

--- pgCocoaDB/Schema.h
+++ pgCocoaDB/Schema.h
@@ -15,19 +15,22 @@
 
 	Connection *connection;
 	NSString *defaultSchemaName;  // "public"
-	
+	NSString *pg_version_found;
 }
 
 - initWithConnection:(Connection *) theConnection;
 
--(RecordSet *)getFunctionInfoFromSchema:(NSString *)schemaName functionName: (NSString *) functionName;
 -(RecordSet *)getFunctionNamesFromSchema:(NSString *)schemaName;
+-(RecordSet *)getFunctionSourceFromSchema:(NSString *)schemaName functionName: (NSString *) functionName;
 -(RecordSet *)getIndexInfoFromSchema:(NSString *)schemaName tableName:(NSString *) tableName indexName:(NSString *) indexName;
 -(RecordSet *)getIndexNamesFromSchema:(NSString *)schemaName tableName:(NSString *) tableName;
 -(RecordSet *)getObjectDescriptionFromSchema:(NSString *)schemaName objectType:(NSString *)objectType objectName:(NSString *)objectName;
 -(RecordSet *)getSchemaNames;
--(RecordSet *)getTableColumnInfoFromSchema:(NSString *)schemaName tableName:(NSString *) tableName;
+-(RecordSet *)getTableColumnInfoFromSchema:(NSString *)schemaName fromTableName:(NSString *) tableName fromColumnName:(NSString *)columnName;
+-(RecordSet *)getTableColumnNamesFromSchema:(NSString *)schemaName fromTableName:(NSString *) tableName;
 -(RecordSet *)getTableNamesFromSchema:(NSString *)schemaName;
+-(RecordSet *)getTriggerInfoFromSchema:(NSString *)schemaName;
+-(RecordSet *)getTriggerNamesFromSchema:(NSString *)schemaName fromTableName:(NSString *) tableName;
 -(RecordSet *)getViewColumnInfoFromSchema:(NSString *)schemaName viewName:(NSString *) viewName;
 -(RecordSet *)getViewNamesFromSchema:(NSString *)schemaName;
 
--- pgCocoaDB/Schema.m
+++ pgCocoaDB/Schema.m
@@ -5,35 +5,67 @@
 //  Created by Neil Tiffin on 2/26/06.
 //  Copyright 2006 Performance Champions, Inc. All rights reserved.
 //
+//	This class provides information about tables, views, triggers,
+//	indexes, schemas, columns, and functions in the PostgreSQL database.
+//
 
 #import "Schema.h"
-
 
 @implementation Schema
 
 - initWithConnection:(Connection *) theConnection
 {
+	NSString *sql;
+	RecordSet * results;
+	Field * versionField;
+	
 	self = [super init];
 	connection = theConnection;
 	[connection retain];
-	defaultSchemaName = [[[NSString alloc] initWithString: @"public"] retain];
+	defaultSchemaName = [[[[NSString alloc] initWithString: @"public"] retain] autorelease];
 	
+	sql = [NSString stringWithFormat:@"%s", "Select version()"];
+	results = [connection execQuery:sql];
+	versionField = [[[results itemAtIndex:0] fields] itemAtIndex:0];
+	pg_version_found = [[[[NSString alloc] initWithString: [versionField value]] retain] autorelease];
+
+	//PostgreSQL 8.1.2 on powerpc-apple-darwin8.5.0, compiled by GCC powerpc-apple-darwin8-gcc-4.0.1 (GCC) 4.0.1 (Apple Computer, Inc. build 5250)
+	if ([pg_version_found rangeOfString:@"PostgreSQL 8.1"].location == NSNotFound)
+	{
+		// TODO not found raise error?
+
+#if PG_COCOA_DEBUG
+		NSLog(@"Did not find PostgreSQL version 8");
+		NSLog(sql);
+		NSLog(pg_version_found);
+#endif
+
+	}
 	return self;
 }
 
 
--(RecordSet *)getFunctionInfoFromSchema:(NSString *)schemaName functionName: (NSString *) functionName
+-(RecordSet *)getFunctionSourceFromSchema:(NSString *)schemaName functionName: (NSString *) functionName
 {
 	NSString *sql;
 	RecordSet * results;
 	
-	sql = [NSString stringWithFormat:@"%s%s%s",
-		"",
-		schemaName,
-		""];
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
+	
+	sql = [NSString stringWithFormat:@"%s'%@''%@'",
+		"SELECT routine_definition FROM information_schema.routines \
+		WHERE routine_schema = ", schemaName,
+		" AND routine_name = ", functionName];
+
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
 
 	results = [connection execQuery:sql];
-	[[results retain] autorelease];
+	[results autorelease];
 	return results;
 }
 
@@ -43,13 +75,21 @@
 	NSString *sql;
 	RecordSet * results;
 	
-	sql = [NSString stringWithFormat:@"%s%s%s",
-		"",
-		schemaName,
-		""];
-
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
+	sql = [NSString stringWithFormat:@"%s'@s'",
+		"SELECT routine_name \
+		FROM information_schema.routines \
+		WHERE routine_schema = ", schemaName];
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
 	results = [connection execQuery:sql];
-	[[results retain] autorelease];
+	[results autorelease];
 	return results;
 }
 
@@ -59,13 +99,21 @@
 	NSString *sql;
 	RecordSet * results;
 	
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
 	sql = [NSString stringWithFormat:@"%s%s%s",
 		"",
 		schemaName,
 		""];
-
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
 	results = [connection execQuery:sql];
-	[[results retain] autorelease];
+	[results autorelease];
 	return results;
 }
 
@@ -75,13 +123,21 @@
 	NSString *sql;
 	RecordSet * results;
 	
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
 	sql = [NSString stringWithFormat:@"%s%s%s",
 		"",
 		schemaName,
 		""];
-
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
 	results = [connection execQuery:sql];
-	[[results retain] autorelease];
+	[results autorelease];
 	return results;
 }
 
@@ -91,13 +147,21 @@
 	NSString *sql;
 	RecordSet * results;
 	
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
 	sql = [NSString stringWithFormat:@"%s%s%s",
 		"",
 		schemaName,
 		""];
-
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
 	results = [connection execQuery:sql];
-	[[results retain] autorelease];
+	[results autorelease];
 	return results;
 }
 
@@ -110,25 +174,62 @@
 	sql = [NSString stringWithFormat:@"%s%%s",
 		"",
 		""];
-
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
 	results = [connection execQuery:sql];
-	[[results retain] autorelease];
+	[results autorelease];
 	return results;
 }
 
 
--(RecordSet *)getTableColumnInfoFromSchema:(NSString *)schemaName tableName:(NSString *) tableName;
+-(RecordSet *)getTableColumnInfoFromSchema:(NSString *)schemaName fromTableName:(NSString *)tableName fromColumnName:(NSString *)columnName;
 {
 	NSString *sql;
 	RecordSet * results;
 	
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
 	sql = [NSString stringWithFormat:@"%s%s%s",
 		"",
 		schemaName,
 		""];
-
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
 	results = [connection execQuery:sql];
-	[[results retain] autorelease];
+	[results autorelease];
+	return results;
+}
+
+
+-(RecordSet *)getTableColumnNamesFromSchema:(NSString *)schemaName fromTableName:(NSString *) tableName;
+{
+	NSString *sql;
+	RecordSet * results;
+	
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
+	sql = [NSString stringWithFormat:@"%s'%@'%s'%@'%s",
+		"SELECT column_name FROM information_schema.columns \
+		WHERE table_schema = ", schemaName,
+		" AND table_name = ", tableName,
+		" ORDER BY column_name ASC"];
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
+	results = [connection execQuery:sql];
+	[results autorelease];
 	return results;
 }
 
@@ -142,9 +243,61 @@
 		schemaName = defaultSchemaName;
 	}
 	sql = [NSString stringWithFormat:@"%s'%@'%s",
-		"SELECT table_name FROM information_schema.tables WHERE table_schema = ",
-		schemaName,
+		"SELECT table_name FROM information_schema.tables WHERE table_schema = ",schemaName,
 		" AND table_type = 'BASE TABLE' ORDER BY table_name ASC"];
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+
+	results = [connection execQuery:sql];
+	[results autorelease];
+	return results;
+}
+
+
+-(RecordSet *)getTriggerInfoFromSchema:(NSString *)schemaName
+{
+	NSString *sql;
+	RecordSet * results;
+	
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
+	sql = [NSString stringWithFormat:@"%s%s%s",
+		"",
+		schemaName,
+		""];
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+
+	results = [connection execQuery:sql];
+	[results autorelease];
+	return results;
+}
+
+
+-(RecordSet *)getTriggerNamesFromSchema:(NSString *)schemaName fromTableName:(NSString *) tableName
+{
+	NSString *sql;
+	RecordSet * results;
+	
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
+	sql = [NSString stringWithFormat:@"%s'%@'%s'%@'",
+		"SELECT trigger_name FROM information_schema.triggers \
+		WHERE event_object_schema =", schemaName, 
+		" AND event_object_table = ", tableName];
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
 	results = [connection execQuery:sql];
 	[results autorelease];
 	return results;
@@ -156,13 +309,21 @@
 	NSString *sql;
 	RecordSet * results;
 
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
 	sql = [NSString stringWithFormat:@"%s%s%s",
 		"",
 		schemaName,
 		""];
 	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
+	
 	results = [connection execQuery:sql];
-	[[results retain] autorelease];
+	[results autorelease];
 	return results;
 }
 
@@ -172,16 +333,21 @@
 	NSString *sql;
 	RecordSet * results;
 	
+	if (schemaName == nil)
+	{
+		schemaName = defaultSchemaName;
+	}
 	sql = [NSString stringWithFormat:@"%s'%@'%s",
-		"SELECT table_name FROM information_schema.tables WHERE table_schema = ",
-		schemaName,
+		"SELECT table_name FROM information_schema.tables WHERE table_schema = ", schemaName,
 		" AND table_type = 'VIEW' ORDER BY table_name ASC"];
+	
+#if PG_COCOA_DEBUG
+	NSLog(sql);
+#endif
 	
 	results = [connection execQuery:sql];
 	[results autorelease];
 	return results;
 }
-
-
 
 @end
