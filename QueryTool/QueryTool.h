833ca28a910ae5580cef418170294e5694c6395e
Neil Tiffin <neilt@users.noreply.github.com>
2006-09-13 23:13:13 +0000

    Add missing global definition.

---
+++ QueryTool/QueryTool.h
@@ -0,0 +1,15 @@
+/*
+ *  QueryTool.h
+ *  Query Tool for PostgresN
+ *
+ *  Created by Neil Tiffin on 8/30/06.
+ *  Copyright 2006 Performance Champions, Inc.. All rights reserved.
+ *
+ */
+
+
+
+// To tell others that parameters are purposely not used and remove compiler warning.
+#define UNUSED_PARAMETER(x) (void)(x)
+
+// Global Project Definitions
